be250d5de370c24abefc3c44b10ae805c1eda06b
Liliane Portela <codeaunt75@users.noreply.github.com>
2020-10-23 13:37:07 -0300

    Andando com a cobrinha

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let quadro = document.getElementById("snake");
 let contexto = quadro.getContext("2d");
 let caixinha = 32;
-var cobra = [{x: 8*caixinha, y: 8* caixinha}];   
+var cobra = [{x: 8*caixinha, y: 8* caixinha}];  
+let direction = "right"; 
 
 function CriarBG(){
     contexto.fillStyle = "lightgreen";
@@ -14,5 +15,27 @@ function CriarCobrinha(){
         contexto.fillRect (cobra[i].x, cobra[i].y, caixinha, caixinha);
     }
 }
-CriarBG();
-CriarCobrinha();
+
+function iniciarJogo(){
+    CriarBG();
+    CriarCobrinha();
+
+    let cobraX = cobra[0].x;
+    let cobraY = cobra[0].y;
+
+    if(direction == "right") cobraX += caixinha;
+    if(direction == "left") cobraX -= caixinha;
+    if(direction == "up") cobraY -= caixinha;
+    if(direction == "down") cobraY += caixinha;
+
+    cobra.pop();
+
+    let cabeca = {
+        x: cobraX,
+        y: cobraY
+    }
+
+    cobra.unshift(cabeca);
+}
+
+let jogo = setInterval(iniciarJogo,100);
