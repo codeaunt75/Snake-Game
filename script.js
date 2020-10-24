1b7c39b20215887b2fa940a86ae94798c500f17b
Liliane Portela <codeaunt75@users.noreply.github.com>
2020-10-24 19:52:52 -0300

    Criando uma comidinha

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let contexto = quadro.getContext("2d");
 let caixinha = 32;
 var cobra = [{x: 8*caixinha, y: 8* caixinha}];  
 let direction = "right"; 
+let comida = {
+    x:Math.floor(Math.random()*15+1) * caixinha,
+    y:Math.floor(Math.random()*15+1) * caixinha
+}
 
 function CriarBG(){
     contexto.fillStyle = "lightgreen";
@@ -16,6 +20,14 @@ function CriarCobrinha(){
     }
 }
 
+function DesenhaComida(){
+
+    contexto.fillStyle = "red";
+    contexto.fillRect (comida.x, comida.y, caixinha, caixinha);
+
+
+}
+
 document.addEventListener('keydown', update);
 
 function update(event){
@@ -27,13 +39,14 @@ function update(event){
 
 function iniciarJogo(){
 
-    if(cobra[0].x > 15 && direction=="right") cobra[0].x= 0;
+    if(cobra[0].x > (15 * caixinha) && direction=="right") cobra[0].x= 0;
     if(cobra[0].x < 0  && direction=="left") cobra[0].x= 16 * caixinha;
-    if(cobra[0].y > 15 && direction=="down") cobra[0].y= 0;
+    if(cobra[0].y > (15 * caixinha) && direction=="down") cobra[0].y= 0;
     if(cobra[0].y < 0  && direction=="up") cobra[0].y= 16 * caixinha;
 
     CriarBG();
     CriarCobrinha();
+    DesenhaComida();
 
     let cobraX = cobra[0].x;
     let cobraY = cobra[0].y;
